f14fd0ecb6f923913c5f4d801a53a3a4e8222273
ChillerDragon <ChillerDragon@gmail.com>
2021-08-19 17:26:51 +0200

    Initial version

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function setup_env() {
+	local version
+	if [ ! -f ~/.sdlvm/version ]
+	then
+		return
+	fi
+	version="$(cat ~/.sdlvm/version)"
+	if [ "$version" == "" ]
+	then
+		return
+	elif [ ! -d ~/.sdlvm/versions/"$version" ]
+	then
+		return
+	fi
+	export PKG_CONFIG_PATH="$HOME/.sdlvm/versions/$version"
+	export LD_LIBRARY_PATH="$HOME/.sdlvm/versions/$version"
+}
+
+setup_env
+
